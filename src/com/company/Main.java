24f879c2449832ee745e4ff152520e0a7b467e56
Paul A Torrillo <pt696@pvhs-lab112-d17.pvsd.org>
2016-09-07 10:24:05 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+        System.out.println("What does this do");
+        //Within paranthesis there is a set of parameters
+    }
+}
